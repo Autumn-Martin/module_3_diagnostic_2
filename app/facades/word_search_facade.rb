300b6efa6d951ac2cb6ded69288f16364f107719
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-30 10:44:20 -0600

    WIP

--- app/facades/word_search_facade.rb
+++ app/facades/word_search_facade.rb
@@ -4,7 +4,7 @@ class WordSearchFacade
     @word = word
   end
 
-  def sentances
+  def sentance
     conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
       faraday.headers["app_key"] = ENV["app_key"]
       faraday.headers["app_id"] = ENV["app_id"]
@@ -13,7 +13,6 @@ class WordSearchFacade
 
     sentance_data = JSON.parse(conn.get("api/v1/entries/en/#{@word}").body, symbolize_names: true)
     binding.pry
-
-    
+    sentance = sentance_data[:results][0][:lexicalEntries][0][:entries][0][:senses][0][:examples][0][:text]
   end
 end
